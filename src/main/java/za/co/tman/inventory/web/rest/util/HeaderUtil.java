9fb8b355280ecaa71e8c7dbcd5c622e54d52b6dc
Andre <kappaj@gmail.com>
2018-06-16 10:10:47 +0200

    Initial application generated by JHipster-5.0.0-beta.2

---
+++ src/main/java/za/co/tman/inventory/web/rest/util/HeaderUtil.java
@@ -0,0 +1,45 @@
+package za.co.tman.inventory.web.rest.util;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpHeaders;
+
+/**
+ * Utility class for HTTP headers creation.
+ */
+public final class HeaderUtil {
+
+    private static final Logger log = LoggerFactory.getLogger(HeaderUtil.class);
+
+    private static final String APPLICATION_NAME = "inventorymoduleApp";
+
+    private HeaderUtil() {
+    }
+
+    public static HttpHeaders createAlert(String message, String param) {
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("X-inventorymoduleApp-alert", message);
+        headers.add("X-inventorymoduleApp-params", param);
+        return headers;
+    }
+
+    public static HttpHeaders createEntityCreationAlert(String entityName, String param) {
+        return createAlert(APPLICATION_NAME + "." + entityName + ".created", param);
+    }
+
+    public static HttpHeaders createEntityUpdateAlert(String entityName, String param) {
+        return createAlert(APPLICATION_NAME + "." + entityName + ".updated", param);
+    }
+
+    public static HttpHeaders createEntityDeletionAlert(String entityName, String param) {
+        return createAlert(APPLICATION_NAME + "." + entityName + ".deleted", param);
+    }
+
+    public static HttpHeaders createFailureAlert(String entityName, String errorKey, String defaultMessage) {
+        log.error("Entity processing failed, {}", defaultMessage);
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("X-inventorymoduleApp-error", "error." + errorKey);
+        headers.add("X-inventorymoduleApp-params", entityName);
+        return headers;
+    }
+}
